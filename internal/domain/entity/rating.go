ddcda189685b24471e2435a30b5fd0277693de2c
YagoSchramm <yrtoiu1515@gmail.com>
2026-04-13 12:48:26 -0300

    fix(3):Fixed the rating product id on entity and dtos

--- internal/domain/entity/rating.go
+++ internal/domain/entity/rating.go
@@ -11,7 +11,7 @@ type Rating struct {
 	UserID      uuid.UUID  `json:"user_id" db:"user_id"`
 	UserName    string     `json:"user_name" db:"user_name"`
 	PurchaseID  uuid.UUID  `json:"purchase_id" db:"purchase_id"`
-	ProdutctID  uuid.UUID  `json:"product_id" db:"product_id"`
+	ProductID   uuid.UUID  `json:"product_id" db:"product_id"`
 	Rating      float32    `json:"rating" db:"rating"`
 	Description string     `json:"description" db:"description"`
 	CreatedAt   time.Time  `json:"created_at"`
