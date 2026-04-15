5614f023c5f0572e6944666005a5344167d3b2bf
YagoSchramm <yrtoiu1515@gmail.com>
2026-04-15 14:44:22 -0300

    chore():New util hashing functions

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	go.uber.org/zap v1.27.1
 )
 
+require golang.org/x/crypto v0.50.0 // indirect
+
 require (
 	github.com/lib/pq v1.12.3
 	go.uber.org/multierr v1.10.0 // indirect
--- internal/domain/util/util.go
+++ internal/domain/util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func GenerateID() *uuid.UUID {
 	id, err := uuid.NewUUID()
@@ -8,4 +11,12 @@ func GenerateID() *uuid.UUID {
 		return nil
 	}
 	return &id
+}
+func GenerateHash(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	passwordHash := string(hashedPassword)
+	return passwordHash, err
+}
+func ComparePassword(hash, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
